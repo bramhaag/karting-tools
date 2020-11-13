e48bd4fdc5b1e3311483fe8a63fce568bc4e4279
Bram Hagens <bramhagens@hotmail.nl>
2020-11-13 19:57:29 +0100

    Add button text

--- src/components/video/controls/control/ButtonControl.tsx
+++ src/components/video/controls/control/ButtonControl.tsx
@@ -1,38 +1,47 @@
-import { IconProp } from '@fortawesome/fontawesome-svg-core'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { Component, h } from 'preact'
-import classNames from 'classnames'
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Component, h } from "preact";
+import classNames from "classnames";
 
 export type ButtonControlProps = {
-    onClick: () => void,
-    icon: IconProp,
-    tooltip: string,
-    text?: string,
-    color?: string
-}
+    onClick: () => void;
+    icon: IconProp;
+    tooltip: string;
+    text?: string;
+    color?: string;
+};
 
 export class ButtonControl extends Component<ButtonControlProps> {
     static defaultProps = {
         color: "",
         text: ""
-    }
-
+    };
 
     render() {
-        const { onClick, icon, tooltip, text, color } = this.props
+        const { onClick, icon, tooltip, text, color } = this.props;
 
-        const buttonClasses = classNames({
-            [`${color}`]: color
-        }, "button")
+        const buttonClasses = classNames(
+            {
+                [`${color}`]: color
+            },
+            "button"
+        );
 
-        const buttonText = !text ? " " + text : ""
+        const buttonText = text ? <span>&nbsp;{text}</span> : "";
 
         return (
             <div className="column is-narrow">
-                <button className={buttonClasses} onClick={onClick} data-tooltip={tooltip}><FontAwesomeIcon icon={icon} />{buttonText}</button>
+                <button
+                    className={buttonClasses}
+                    onClick={onClick}
+                    data-tooltip={tooltip}
+                >
+                    <FontAwesomeIcon icon={icon} />
+                    {buttonText}
+                </button>
             </div>
-        )
+        );
     }
 }
 
-export default ButtonControl
+export default ButtonControl;
